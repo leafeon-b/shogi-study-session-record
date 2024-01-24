68a1baf83603437c8e5a2db24c7e8cfd744c6351
leafeon-b <grad.f.ishida@gmail.com>
2024-01-24 14:25:15 +0900

    ヘッダにサインイン/サインアウト機能の実装

--- src/components/auth/Navigation.tsx
+++ src/components/auth/Navigation.tsx
@@ -1,6 +1,7 @@
-"use client";
-
 import Link from "next/link";
+import { getServerAuthSession } from "~/server/auth";
+
+const session = await getServerAuthSession();
 
 // ナビゲーション
 const Navigation = () => {
@@ -9,6 +10,12 @@ const Navigation = () => {
       <div className="container mx-auto flex max-w-screen-md items-center justify-between px-2 py-3">
         <Link href="/" className="cursor-pointer text-xl font-bold">
           将棋研究会記録帳
+        </Link>
+        <Link
+          href={session ? "/api/auth/signout" : "/api/auth/signin"}
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        >
+          {session ? "Sign out" : "Sign in"}
         </Link>
       </div>
     </header>
