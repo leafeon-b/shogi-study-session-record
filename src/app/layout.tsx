76fc9a8d2b8a8ce3c3ff6ddb73e931697647d83d
leafeon-b <grad.f.ishida@gmail.com>
2024-01-26 10:10:25 +0900

    トップページに研究会一覧の表示

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 
 config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
@@ -25,27 +26,29 @@ const RootLayout = async ({ children }: RootLayoutProps) => {
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <div className="flex min-h-screen flex-col">
-          <Navigation />
+        <AppRouterCacheProvider>
+          <div className="flex min-h-screen flex-col">
+            <Navigation />
 
-          <main className="container mx-auto max-w-screen-md flex-1 px-2">
-            {children}
-          </main>
+            <main className="container mx-auto max-w-screen-md flex-1 px-2">
+              {children}
+            </main>
 
-          {/* フッター */}
-          <footer className="py-5">
-            <div className="text-center text-sm">
-              Copyright © All rights reserved |{" "}
-              <Link
-                href="https://github.com/leafeon-b"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-              </Link>
-            </div>
-          </footer>
-        </div>
+            {/* フッター */}
+            <footer className="py-5">
+              <div className="text-center text-sm">
+                Copyright © All rights reserved |{" "}
+                <Link
+                  href="https://github.com/leafeon-b"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGithub} size="2x" />
+                </Link>
+              </div>
+            </footer>
+          </div>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { unstable_noStore as noStore } from "next/cache";
+import { api } from "~/trpc/server";
 
-export default async function Home() {
+export default function Home() {
   noStore();
 
+  return <HomePage />;
+}
+
+async function HomePage() {
+  const data = await api.studyGroup.getAll.query();
+
   return (
-    <div>
-      <div>Main</div>
-    </div>
+    <main className="flex h-screen justify-center">
+      <div className="h-full w-full border-x md:max-w-2xl">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>名前</TableCell>
+              <TableCell>説明</TableCell>
+              <TableCell>作成日</TableCell>
+              <TableCell>最終更新日</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.description}</TableCell>
+                <TableCell>{item.createdAt.toLocaleDateString()}</TableCell>
+                <TableCell>{item.updatedAt.toLocaleDateString()}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </main>
   );
 }
